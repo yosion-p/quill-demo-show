47198b0edbde7a1950cf2dcca1c80c886c7d30a9
yosion-p <yosion2012@163.com>
2022-02-16 15:59:49 +0800

    counter

--- show-demo/src/main.ts
+++ show-demo/src/main.ts
@@ -2,20 +2,31 @@ import './style.css'
 
 // 引入
 import Quill from 'quill';
+import Counter from './counter';
+Quill.register('modules/counter', Counter);
 
 // 使用
-// const quill = new Quill('#editor');
-
 var editor = new Quill('#editor', {
   modules: {
-      toolbar: ['bold', 'italic']
+    toolbar: ['bold', 'italic'],
+    counter: true,
+    // 新增-一个添加删除线的快捷键
+    keyboard: {
+      bindings: {
+        strike: {
+          key: 'X',
+          ctrlKey: true,
+          shiftKey: true,
+          handler: function (range, context) {
+            // 获取当前光标所在文本的格式
+            const format = this.quill.getFormat(range);
+            // 增加/取消删除线
+            this.quill.format('strike', !format.strike);
+          }
+        },
+      },
+    },
   },
   theme: 'snow',
 });
 
-// const app = document.querySelector<HTMLDivElement>('#app')!
-
-// app.innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
